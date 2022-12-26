a9f3422eb16c3ad995babcd280101e5107f8043e
KaveyZheng <kaveyzheng1@gmail.com>
2022-12-26 11:17:52 -0500

    Initial Commit

---
+++ IntroduceStudent/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IntroduceStudent
+//
+//  Created by Kavey Zheng on 12/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
